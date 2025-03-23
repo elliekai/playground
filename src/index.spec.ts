cb7e010fdf1aceb91e632bc2baf28527a521b83c
Ellie Kai <ellie.p.kai@gmail.com>
2025-03-23 01:16:27 -0400

    chore: wrap tests for base case label

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,8 @@
-import { expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import playground from "./index.js";
 
-it("should return a result object", () => {
-  expect(playground()).toEqual({ message: "Hello World!" });
+describe("base cases", () => {
+  it("should return a result object", () => {
+    expect(playground()).toEqual({ message: "Hello World!" });
+  });
 });
